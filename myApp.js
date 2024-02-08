62288ea095cad4210a3ab4892d208627c353feb7
Replit user <>
2024-02-08 06:44:50 +0000

    add middleware express.static function

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require("express");
 let app = express();
 console.log("Hello World");
 
-app.use(express.static(__dirname + "/public/"));
+app.use(express.static(__dirname + "/"));
 const absolutePath = __dirname + "/views/index.html";
 app.get("/public", (req, res) => res.sendFile(absolutePath));
 
