96b569d1176f612ad9495f12b72e8358733da289
Replit user <>
2024-02-08 06:20:07 +0000

    update response into file path

--- myApp.js
+++ myApp.js
@@ -1,40 +1,9 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-console.log("Hello World")
-const absolutePath = __dirname + '/views/index.html'
-app.get("/", (req, res) => res.sendFile(absolutePath))
+console.log("Hello World");
+const absolutePath = __dirname + "/views/index.html";
+app.get("/", (req, res) => res.sendFile(absolutePath));
 
+app.use(express.static(__dirname + "/public"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
